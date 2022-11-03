81ee5506a7b518b2256278c1231b86c2ececf6a1
csmailis <22669824+csmailis@users.noreply.github.com>
2022-11-03 09:57:14 -0500

    Update heightmap_example.py

--- code_for_generating_npy_samples/heightmap_example.py
+++ code_for_generating_npy_samples/heightmap_example.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jun 20 10:13:19 2021
 
-@author: user
-"""
 from PIL import Image
 import numpy as np
 heightmap = Image.open("Heightmap.png")
